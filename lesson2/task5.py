9629c9e4c5bf4c1aa412774d1d21137981724204
aliaksandr <prokopenya_a@mail.ru>
2024-02-10 13:24:24 +0300

    5th task delete whitespace

--- lesson2/task5.py
+++ lesson2/task5.py
@@ -3,7 +3,7 @@
 """
 
 number = int(input("Введите двузначное число "))
-if  10 < number > 100:
+if 10 < number > 100:
     print("Неверный ввод")
 elif 10 <= ((number // 10) + (number % 10)) < 100:
     print("Да")
