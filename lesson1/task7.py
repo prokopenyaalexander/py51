44cb5f473c820c576d311222648186dadada6ccc
aliaksandr <prokopenya_a@mail.ru>
2024-02-10 12:55:10 +0300

    2nd task

--- lesson1/task7.py
+++ lesson1/task7.py
@@ -6,8 +6,8 @@ Sample Output :
 Можно отрезать 10 квадратов.
 """
 
-side_1 = 150
-side_2 = 65
+side_1 = 165
+side_2 = 25
 square = 30
 result = side_1 * side_2 // 30 ** 2
 print(f'Можно отрезать {result} квадратов')
