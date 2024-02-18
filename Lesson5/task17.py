ad7e4289b90f72eaa3c65f9657b77e77716d58b4
aliaksandr <prokopenya_a@mail.ru>
2024-02-18 16:43:43 +0300

    17th task

--- Lesson5/task17.py
+++ Lesson5/task17.py
@@ -1,5 +1,6 @@
 """
 Удалить в строке все буквы 'b', непосредственно за которыми идет цифра.
+Удалить из текста символы, являющиеся строчными латинскими буквами.
 """
 test_string = input('Введите строку ')
 temp = list(test_string)
@@ -8,6 +9,15 @@ for char in temp:
         index = temp.index('b')
         if temp[index + 1].isdigit():
             temp.remove('b')
+print(f'Строка без символа b: {''.join(temp)}')
 
-print(''.join(temp))
+new_text = ''
+letters = []
+for letter in range(ord('A'), ord('Z')+1):
+    letters.append(chr(letter))
+
+for char in test_string:
+    if not (char.isalpha() and char in letters and char.isupper()):
+        new_text += char
+print(f'Измененный текст {new_text}')
 
