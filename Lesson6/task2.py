b15409a0eb40e84271ab88967f0ce504283462eb
aliaksandr <prokopenya_a@mail.ru>
2024-02-19 22:58:20 +0300

    2nd task

---
+++ Lesson6/task2.py
@@ -0,0 +1,20 @@
+"""
+2.	Найдите минимальное 9 чисел с помощью функции написанной в предыдущей  задаче. Новую функцию не создавать! Использовать функцию из предыдущей задачи!
+"""
+
+
+def minimum(*args):
+    min_number = 0
+    for i in args[0]:
+        if min_number > float(i):
+            min_number = i
+    print(f'Минимальное число {min_number}')
+
+
+temp_lst = []
+print("Введите числа")
+for num in range(3):
+    number = float(input())
+    temp_lst.append(number)
+
+minimum(temp_lst)
