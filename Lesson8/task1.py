b442094ab1386a6b4b5ec20e5e4f36f02c153789
aliaksandr <prokopenya_a@mail.ru>
2024-02-27 21:31:27 +0300

    1st rewrote task

--- Lesson8/task1.py
+++ Lesson8/task1.py
@@ -9,8 +9,8 @@ def check_index(lst):
     try:
         index = int(input("Введите индекс элемнта, который хотите вывести "))
         return f'Массив {lst}\nВведнный индекс {index} - эелемент {lst[index]}'
-    except IndexError:
-        return f'Ошибка - {IndexError}'
+    except IndexError as error :
+        return f'Ошибка - {error}'
 
 
 print(check_index(some_lst))
