c6f9317425152de3acec6d62a99411505b0c45a0
aliaksandr <prokopenya_a@mail.ru>
2024-02-27 20:54:30 +0300

    1st task

---
+++ Lesson8/task1.py
@@ -0,0 +1,16 @@
+"""
+1.	Напишите программу которые будет ловить IndexError, когда вы пытаетесь взять индекс элемента, которого нет в списке.
+"""
+import random
+some_lst = [random.randint(1, 20) for value in range(10)]
+
+
+def check_index(lst):
+    try:
+        index = int(input("Введите индекс элемнта, который хотите вывести "))
+        return f'Массив {lst}\nВведнный индекс {index} - эелемент {lst[index]}'
+    except IndexError:
+        return f'Ошибка - {IndexError}'
+
+
+print(check_index(some_lst))
